e97032c4d73606f14e188742703ef7a76f4eb8d6
Paolo Abeni <pabeni@redhat.com>
2024-05-24 08:45:31 +0200

    shapers: spec: Add Netlink speic in YAML
    Define the user-space visible interface to query, configure and delete
    network shapers via yaml definition.
    
    Add dummy implementations for the relevant NL callbacks.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    ---
    v2 -> v3:
     - dropped 'move' op, use parent in 'set' instead
     - expand 'handle' in 'scope', 'major', 'minor'
     - rename 'queue_group' scope to 'detached'
     - rename 'info' attr to 'shapers'
     - added pad attribute (for 64 bits' sake)
    v1 -> v2:
     - reset -> delete
     - added 'parent' and 'burst'

---
+++ include/uapi/linux/net_shaper.h
@@ -0,0 +1,72 @@
+/* SPDX-License-Identifier: ((GPL-2.0 WITH Linux-syscall-note) OR BSD-3-Clause) */
+/* Do not edit directly, auto-generated from: */
+/*	Documentation/netlink/specs/shaper.yaml */
+/* YNL-GEN uapi header */
+
+#ifndef _UAPI_LINUX_NET_SHAPER_H
+#define _UAPI_LINUX_NET_SHAPER_H
+
+#define NET_SHAPER_FAMILY_NAME		"net_shaper"
+#define NET_SHAPER_FAMILY_VERSION	1
+
+/**
+ * enum net_shaper_scope - the different scopes where a shaper could be
+ *   attached
+ * @NET_SHAPER_SCOPE_PORT: The root shaper for the whole H/W.
+ * @NET_SHAPER_SCOPE_NETDEV: The main shaper for the given network device.
+ * @NET_SHAPER_SCOPE_QUEUE: The shaper is attached to the given device queue.
+ * @NET_SHAPER_SCOPE_DETACHED: The shaper can be attached to port, netdev or
+ *   other detached shapers, allowing nesting and grouping of netdev or queues.
+ */
+enum net_shaper_scope {
+	NET_SHAPER_SCOPE_PORT,
+	NET_SHAPER_SCOPE_NETDEV,
+	NET_SHAPER_SCOPE_QUEUE,
+	NET_SHAPER_SCOPE_DETACHED,
+
+	/* private: */
+	__NET_SHAPER_SCOPE_MAX,
+	NET_SHAPER_SCOPE_MAX = (__NET_SHAPER_SCOPE_MAX - 1)
+};
+
+/**
+ * enum net_shaper_metric - different metric each shaper can support
+ * @NET_SHAPER_METRIC_PPS: Shaper operates on a packets per second basis
+ * @NET_SHAPER_METRIC_BPS: Shaper operates on a bits per second basis
+ */
+enum net_shaper_metric {
+	NET_SHAPER_METRIC_PPS,
+	NET_SHAPER_METRIC_BPS,
+};
+
+enum {
+	NET_SHAPER_A_PARENT = 1,
+	NET_SHAPER_A_HANDLE,
+	NET_SHAPER_A_METRIC,
+	NET_SHAPER_A_BW_MIN,
+	NET_SHAPER_A_BW_MAX,
+	NET_SHAPER_A_BURST,
+	NET_SHAPER_A_PRIORITY,
+	NET_SHAPER_A_WEIGHT,
+	NET_SHAPER_A_SCOPE,
+	NET_SHAPER_A_MAJOR,
+	NET_SHAPER_A_MINOR,
+	NET_SHAPER_A_IFINDEX,
+	NET_SHAPER_A_HANDLES,
+	NET_SHAPER_A_SHAPERS,
+	NET_SHAPER_A_PAD,
+
+	__NET_SHAPER_A_MAX,
+	NET_SHAPER_A_MAX = (__NET_SHAPER_A_MAX - 1)
+};
+
+enum {
+	NET_SHAPER_CMD_GET = 1,
+	NET_SHAPER_CMD_SET,
+	NET_SHAPER_CMD_DELETE,
+
+	__NET_SHAPER_CMD_MAX,
+	NET_SHAPER_CMD_MAX = (__NET_SHAPER_CMD_MAX - 1)
+};
+
+#endif /* _UAPI_LINUX_NET_SHAPER_H */
